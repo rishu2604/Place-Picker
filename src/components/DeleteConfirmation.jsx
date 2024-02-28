b7f2ebbe2d50685ac253182d084ffad09b48a28a
rishu2604 <anand.rishu310@gmail.com>
2024-02-28 17:06:23 +0530

    "useEffect Cleanup"

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(()=>{
+    console.log('tmr set');
+    const timer = setTimeout(()=>{
+      onConfirm();
+    }, 4000);
+
+    return () => {
+      console.log('tm clear');
+      clearTimeout(timer);
+    };
+  }, [onConfirm]); // if you are using prop or state value in your effect function, you should add them as dependencies
+  // But there is a possibility of infinite loop while adding functions as dependencies.
+  // Because functions are treated as objects in javascript and javascript does not treat two objects as equal even if all the values are same in those objects
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
